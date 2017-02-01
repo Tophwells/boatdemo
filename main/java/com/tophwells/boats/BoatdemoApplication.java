5567b338fbd13eedf2b103b23ef9b21d80b6d2d5
Christopher Wells <toph.wells@gmail.com>
2017-02-01 17:21:57 +0100

    Initial commit.

---
+++ main/java/com/tophwells/boats/BoatdemoApplication.java
@@ -0,0 +1,12 @@
+package com.tophwells.boats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatdemoApplication.class, args);
+	}
+}
---
+++ test/java/com/tophwells/boats/BoatdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tophwells.boats;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoatdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
